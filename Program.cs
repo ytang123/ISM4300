fc1340ad1f282db25d9ccc17ffa49e147cae33cf
ytang123 <89617499+ytang123@users.noreply.github.com>
2021-08-31 21:18:13 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Stuck_In_A_Timeloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            for (int y = 1; y <= x; y++)
+            {
+                Console.WriteLine(y + " Abracadabra ");
+            }
+        }
+    }
+}
