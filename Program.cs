eea7e21095839ccce59fba53f1eb9eeda116f349
ytang123 <89617499+ytang123@users.noreply.github.com>
2021-09-18 13:32:08 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,16 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-namespace Stuck_In_A_Timeloop
+namespace Election1
 {
     class Program
     {
+        private static object dictionary;
+        private static object input;
+
         static void Main(string[] args)
         {
-            int x = int.Parse(Console.ReadLine());
-            for (int y = 1; y <= x; y++)
+            // Declare dictionary for candiate and votes
+            var cans = new Dictionary<string, string>();
+            var votes = new Dictionary<string, int>();
+
+            int x = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < x; i++)
             {
-                Console.WriteLine(y + " Abracadabra ");
+                cans.Add(Console.ReadLine(), Console.ReadLine());
             }
-        }
+
+            int voteNum = Int32.Parse(Console.ReadLine());
+
+       
     }
 }
+
+               
